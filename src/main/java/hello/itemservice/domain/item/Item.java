e1ae716b9b3979d3d5fb267ca5bbcc660617af58
jurio5 <sia5492@naver.com>
2025-04-26 22:10:05 +0900

    [refactor] 수정 요구사항, 사이드 이펙트 터짐

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,7 +3,6 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -11,6 +10,7 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량의 합이 10,000원이 넘도록 입력해주세요.")
 public class Item {
 
+    @NotNull // 수정 요구사항
     private Long id;
 
     @NotBlank
@@ -21,7 +21,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+//    @Max(9999) // 수정 요구사항
     private Integer quantity;
 
     public Item() {
